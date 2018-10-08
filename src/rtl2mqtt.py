5a03c36f7292c25386c9da4138d1fa633c1b1cd4
David Inman <david_inman@apple.com>
2018-10-08 17:29:39 -0500

    updates for PEP8

--- src/rtl2mqtt.py
+++ src/rtl2mqtt.py
@@ -1,71 +1,87 @@
 #!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
 
 import subprocess
-import sys
-import time
+# import sys
+# import time
 import paho.mqtt.client as mqtt
-import os
+# import os
 import json
 
-from config import *
+from config import (
+    mqtt_user, mqtt_pass, mqtt_host,
+    mqtt_port, mqtt_topic, mqtt_qos, freq
+)
 
-rtl_433_cmd = "/usr/local/bin/rtl_433 -G -F json" # linux
+rtl_433_cmd = "/usr/local/bin/rtl_433 -f {0} -G -F json".format(freq)  # linux
+
 
 # Define MQTT event callbacks
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
+
 def on_disconnect(client, userdata, rc):
     if rc != 0:
         print("Unexpected disconnection.")
 
+
 def on_message(client, obj, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
 
 def on_publish(client, obj, mid):
     print("mid: " + str(mid))
 
+
 def on_subscribe(client, obj, mid, granted_qos):
     print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
 
 def on_log(client, obj, level, string):
     print(string)
 
-# Setup MQTT connection
 
+# Setup MQTT connection
 mqttc = mqtt.Client()
 # Assign event callbacks
-#mqttc.on_message = on_message
+# mqttc.on_message = on_message
 mqttc.on_connect = on_connect
-#mqttc.on_publish = on_publish
+# mqttc.on_publish = on_publish
 mqttc.on_subscribe = on_subscribe
 mqttc.on_disconnect = on_disconnect
 
 # Uncomment to enable debug messages
-#mqttc.on_log = on_log
+# mqttc.on_log = on_log
 
 # Uncomment the next line if your MQTT server requires authentication
-#mqttc.username_pw_set(MQTT_USER, password=MQTT_PASS)
-mqttc.connect(MQTT_HOST, MQTT_PORT, 60)
+mqttc.username_pw_set(mqtt_user, password=mqtt_pass)
+mqttc.connect(mqtt_host, mqtt_port, 60)
 
 mqttc.loop_start()
 
 # Start RTL433 listener
-rtl433_proc = subprocess.Popen(rtl_433_cmd.split(),stdout=subprocess.PIPE,stderr=subprocess.STDOUT,universal_newlines=True)
+rtl433_proc = subprocess.Popen(
+    rtl_433_cmd.split(),
+    stdout=subprocess.PIPE,
+    stderr=subprocess.STDOUT,
+    universal_newlines=True)
 
 
 while True:
     for line in iter(rtl433_proc.stdout.readline, '\n'):
         if "time" in line:
-            mqttc.publish(MQTT_TOPIC, payload=line,qos=MQTT_QOS)
+            mqttc.publish(
+                mqtt_topic, payload=line, qos=mqtt_qos)
             json_dict = json.loads(line)
             for item in json_dict:
                 value = json_dict[item]
                 if "model" in item:
-                    subtopic=value
+                    subtopic = value
 
             for item in json_dict:
                 value = json_dict[item]
-                if not "model" in item:
-                    mqttc.publish(MQTT_TOPIC+"/"+subtopic+"/"+item, payload=value,qos=MQTT_QOS)
+                if "model" not in item:
+                    mqttc.publish(
+                        mqtt_topic+"/"+subtopic+"/"+item,
+                        payload=value,
+                        qos=mqtt_qos)
